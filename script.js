3ee1386bf051a8c70ae73741598d6f35089e7a24
muskan179 <55253187+muskan179@users.noreply.github.com>
2020-03-10 20:24:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function myFunction(x) {
+  x.classList.toggle("change");
+  document.getElementById('myDropdown').style.display = document.getElementById('myDropdown').style.display =='block' ?   'none' : 'block';
+}
